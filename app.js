9f5616826f6770de9bf9b277f4a665ff9c047ba9
Gitmannen1337 <56875916+Gitmannen1337@users.noreply.github.com>
2020-05-04 11:47:18 +0200

    Work in progress

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
-import dropmenu from './component/dropmenu.js'
+// import dropmenu from './component/dropmenu.js'
+import getEventsNearby from './getEventsNearby.js'
 
-Vue.component('drop-items', dropmenu) 
+// Vue.component('drop-items', dropmenu) 
 
 // ===== Want this in own file ======
-// class Link {
-//     constructor(name, href) {
-//     this.name = name
-//     this.href = href
-//     }
-//     }
-//     Vue.component('link-items', {
-//     props: {items: Array},
-//     template: `<div>
-//     <a v-for="link in items" :href="link.href">{{link.name}}
-//     </a>
-//     </div>`
-//     })
+class Link {
+    constructor(name, href) {
+        this.name = name
+        this.href = href
+    }
+}
+Vue.component('link-items', {
+    props: { items: Array },
+    template: `<div>
+     <a v-for="link in items" :href="link.href">{{link.name}}
+     </a>     </div>`
+})
+
+Vue.component('events-component', {
+    props: { event : Array },
+    template: `<div>
+     <p v-for="event in items">{{event}}
+     </p>     </div>`
+})
 // ====================================
 
 
 let app = new Vue({
     el: '#app',
     data: {
-    // Items for Link
-     linkitems: [ new Link('Hem', 'index.html'),
-                new Link('Om oss', 'about.html') ]
-    // Items for DropmenuItems   
+        events: await getEventsNearby(59.32, 18.06),
+        // Items for Link
+              linkitems: [new Link('Hem', 'index.html'),
+        new Link('Om oss', 'about.html')]
+    },
+    methods: {
+        // Method for button 'Brott i närheten', ska kopplas till den riktiga metoden
+    //     getEventsNearby: async function (event) {
+            
+    // }
+        }
     
-},
-methods: {
-    // Method for button 'Brott i närheten', ska kopplas till den riktiga metoden
-    getEventsNearby: function (event) {
-        console.log('ring aina')
-    }
-}
 })
